5c9147a670486c30182527858d2986ed8e49c8ee
An Ki <kiyko.andriy@gmail.com>
2021-11-03 20:43:04 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
-// import calc from './modules/calc'
+import calc from './modules/calc'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
@@ -9,7 +9,7 @@ import slider from './modules/slider'
 timer('7 january 2022');
 menu();
 modal();
-// calc();
+calc(100, 1);
 validation();
 tabs();
 slider('portfolio-content', 'portfolio-item', 'portfolio-dots', 'dot', 'portfolio-btn');
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,6 +1,60 @@
-const calc = function () {
+const calc = function (price = 100, animationTime = 2) {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcType = document.querySelector('.calc-type');
+    const calcSquare = document.querySelector('.calc-square');
+    const calcCount = document.querySelector('.calc-count');
+    const calcDay = document.querySelector('.calc-day');
+    const total = document.getElementById('total')
+
+    const countCalc = function () {
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value
+        const calcSquareValue = +calcSquare.value;
+        console.log(calcSquareValue)
 
 
+        let totalValue = 0;
+        let calcCountValue = 1;
+        let calcDayValue = 1;
+
+        if (calcCount.value > 1) {
+            calcCountValue += +calcCount.value / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+            calcDayValue = 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+            calcDayValue = 1.5;
+        }
+
+        if (calcTypeValue && calcSquareValue) {
+            totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+            animate(totalValue, animationTime);
+        } else if (calcSquareValue == 0) {
+            total.textContent = 0;
+        }
+
+        function animate(value, time) {
+            let count = time * 25;
+            const newValue = Math.round(value / count);
+            let totalSum = 0;
+            requestAnimationFrame(function anim() {
+                    totalSum += newValue;
+                    total.textContent = totalSum;
+                    if (totalSum > totalValue) {
+                        total.textContent = totalValue;
+                    } else {
+                        requestAnimationFrame(anim)
+                    }
+                }
+            )
+        }
+    }
+
+    calcBlock.addEventListener('input', (e) => {
+        if (e.target === calcType || e.target === calcSquare ||
+            e.target === calcCount || e.target === calcDay) {
+            countCalc()
+        }
+    })
 }
-
 export default calc;
