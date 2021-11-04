3da5b044dbd84a322e0ac94dc1b7e5216942d218
An Ki <kiyko.andriy@gmail.com>
2021-11-04 23:05:17 +0100

    added animate to menu_js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -13,10 +13,43 @@ const toggleMenu = function () {
     //     }
     // })
 
+    const animate = function ({timing, draw, duration}) {
+
+        let start = performance.now();
+
+        requestAnimationFrame(function animate(time) {
+            // timeFraction изменяется от 0 до 1
+            let timeFraction = (time - start) / duration;
+            if (timeFraction > 1) timeFraction = 1;
+
+            let progress = timing(timeFraction);
+
+            draw(progress);
+
+            if (timeFraction < 1) {
+                requestAnimationFrame(animate);
+            }
+        });
+    }
+
     body.addEventListener('click', (e) => {
         if (e.target.matches('a') && e.target.closest('menu')) {
             if (e.target.classList.contains('close-btn')) {
                 e.preventDefault()
+            } else {
+                e.preventDefault();
+                let targetElement = document.querySelector(e.target.hash);
+                let elem = targetElement.getBoundingClientRect();
+                let result = elem.top + window.pageYOffset;
+                animate({
+                    duration: 778,
+                    timing(timeFraction) {
+                        return Math.pow(timeFraction, 12)
+                    },
+                    draw(progress) {
+                        document.documentElement.scrollTop = progress * result;
+                    }
+                });
             }
             handleMenu()
         } else if (e.target.closest('.menu')) {
